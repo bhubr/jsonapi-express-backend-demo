48f77fc8560471840ba70c900da51be175bf1bc3
Benoît Hubert <benoithubert@gmail.com>
2017-04-13 12:38:18 +0200

    Implement contact form email delivery through Mailgun

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This project aims to demonstrate the use of my [JSONAPI Express backend project](https://github.com/bhubr/jsonapi-express-backend).
 
-## Installation
+## Installation and setup
 
 ### 1. Install dependencies
     npm install
@@ -13,11 +13,17 @@ Those keys will be used for JSON Web Token generation and checking.
 
     node keys/keygen
 
-### 3. Modify config.json and create database
+### 3. Copy config.sample.json to config.json 
+
+First, you **must** copy `config.sample.json` to `config.json`.
 
 The server app can run in three environments: `development`, `test`, `production`. Each has a corresponding key in `config.json`. Default environment is `development` and can be overrided by setting `NODE_ENV` before lauching the app.
 
-You must modify entries in relevant `config.json` section to match your MySQL database settings. Of course you must **create the database** and **inject the schema** (`schema.sql`) before running the app.
+You must modify entries in relevant `config.json` section to match your MySQL database settings. 
+
+### 4. Create database
+
+Of course you must **create the database**, through phpMyAdmin or mysql console, and **inject the schema** (`schema.sql`) before running the app.
 
 ## Launch!
 
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var config = configs[env];
 
 var models = require('./models');
 var { router, middlewares } = require('jsonapi-express-backend')(__dirname, config, models);
+ // var { router, middlewares } = require('../jsonapi-reference')(__dirname, config, models);
 
 var port = process.argv.length >= 3 ? parseInt( process.argv[2], 10 ) : 3002;
 
@@ -20,6 +21,38 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(middlewares.checkJwt);
 app.use(middlewares.jsonApi);
 app.use('/api/v1', router);
+
+app.post('/send-message', (req, res) => {
+  const nodemailer = require('nodemailer');
+  let transporter = nodemailer.createTransport({
+    service: 'Mailgun',
+    auth: {
+      user: config.email.mailgunLogin,
+      pass: config.email.mailgunPass
+    }
+  });
+
+  const from = 'postmaster@ember-blog.info';
+  const replyTo = req.body.name + ' <' + req.body.email + '>';
+  const to = config.email.adminEmail;
+  const subject = 'Message from ' + replyTo;
+  const text = req.body.message;
+  const html = req.body.message.replace(/[\n\r]/g, '<br>');
+  // setup email data with unicode symbols
+  let mailOptions = { from, to, replyTo, subject, text, html };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return res.status(500).send(error);
+      }
+      console.log('Message %s sent: %s', info.messageId, info.response);
+      const { messageId, response } = info;
+      return res.json({ mailOptions, messageId, response });
+  });
+
+  // return res.json(req.body);
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port ' + port);
